6675f876f5b5d5ab14d2de713b46207895d29a25
Tim <tim.vandijk@student.hu.nl>
2019-04-06 13:28:53 +0200

    changed conversion formula a bit

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -6,7 +6,7 @@ IntensityImage *StudentPreProcessing::stepToIntensityImage(const RGBImage &image
     IntensityImageStudent *new_img = new IntensityImageStudent(image.getWidth(), image.getHeight());
     for (int i = 0; i < image.getWidth() * image.getHeight(); i++) {
         RGB pixel = image.getPixel(i);
-        new_img->setPixel(i, pixel.r * 0.3f + pixel.g * 0.6f + pixel.b * 0.1f);
+        new_img->setPixel(i, pixel.r * 0.3f + pixel.g * 0.59f + pixel.b * 0.11f);
     }
 
     return new_img;
